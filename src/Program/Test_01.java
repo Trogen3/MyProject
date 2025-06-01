5a3d1445a7df97411b89ca736c851711bb3f2b69
Trogen3 <kiratdehade3@gmail.com>
2025-06-01 15:59:53 +0530

    update

--- src/Program/Test_01.java
+++ src/Program/Test_01.java
@@ -1,8 +1,20 @@
 package Program;
 
+import java.util.Scanner;
+
 public class Test_01 {
 	
 public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter number");
+	int num=sc.nextInt();
+	int rev=0;
+	
+	while(num!=0) {
+		rev=rev*10+num%10;
+		num=num/10;
+	}
+	System.out.println(rev);
 	
 }
 }
